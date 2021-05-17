571077872d942c312549faadff874bc413b79473
pdenieves <pedro_de_nieves@yahoo.com>
2021-05-17 20:18:25 +0200

    cambios rama_modelo

--- script_entrenamiento.py
+++ script_entrenamiento.py
@@ -7,3 +7,4 @@ df = pd.read_csv("fichero.csv", sep = ";")
 x_train, x_test, y_train, y_test = train_test_split(df['texto'], df['etiqueta'], train_size = 0.8)
 
 ## Añadir el entrenamiento de un modelo de regresion lineal
+##
